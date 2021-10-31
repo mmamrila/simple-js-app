c0004fbb38f3b61bda5b34caae2259b1e6643c88
Mike Mamrila <mmamrila@gmail.com>
2021-10-31 16:24:26 -0600

    Added pokemonlist object and first 3 pokemon arrays

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
-let myName = 'Bob';
-document.write(myName);
-myName = 'John Doe';
-document.write(myName);
+let pokemonList = [
+  {name: 'Gengar', height: '1.5', type: ['ghost', 'poison']},
+  {name: 'Scyther', height: '1.5', type: ['bug', 'flying']},
+  {name: 'Tauros', height: '1.4', type: 'normal'}
+];
