366cc3617e640a364dea10b5fdb7289252712a40
Mike Mamrila <mmamrila@gmail.com>
2021-11-03 19:19:36 -0600

    Added for loop to post pokemon name and height

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 let pokemonList = [
   {name: 'Gengar', height: '1.5', type: ['ghost', 'poison']},
-  {name: 'Scyther', height: '1.5', type: ['bug', 'flying']},
-  {name: 'Tauros', height: '1.4', type: 'normal'}
+  {name: 'Onix', height: '8.8', type: ['rock', 'ground']},
+  {name: 'Magnemite', height: '0.3', type: ['electric', 'steel']}
 ];
+
+for (let i=0; i < pokemonList.length; i++) {
+  document.write(pokemonList[i].name + ': ' + '(height: ' + pokemonList[i].height + ')');
+}
