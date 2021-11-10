867ab17636563d61a7f97f6d34e898c819ddd877
Mike Mamrila <mmamrila@gmail.com>
2021-11-09 19:37:47 -0700

    IIFE update

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,26 @@
-let pokemonList = [
-  {name: 'Gengar', height: '1.5', type: ['ghost', 'poison']},
-  {name: 'Onix', height: '8.8', type: ['rock', 'ground']},
-  {name: 'Magnemite', height: '0.3', type: ['electric', 'steel']}
-];
+let pokemonRepository = (function () {
+  let pokemonList = [
+    {name: 'Gengar', height: '1.5', type: ['ghost', 'poison']},
+    {name: 'Onix', height: '8.8', type: ['rock', 'ground']},
+    {name: 'Magnemite', height: '0.3', type: ['electric', 'steel']}
+  ];
+
+  function add(pokemon) {
+    pokemonList.push(pokemon);
+  }
+
+  function getAll() {
+    return pokemonList;
+  }
+
+  return {
+    add: add,
+    getAll: getAll
+  };
+})();
 
 //Write's pokemon's name and height in DOM
-pokemonList.forEach(function(pokemon) {
+pokemonRepository.getAll().forEach(function(pokemon) {
   document.write('<br>' + pokemon.name + ': ' + '(height: ' + pokemon.height + ')');
 
   if (pokemon.height > 5){
